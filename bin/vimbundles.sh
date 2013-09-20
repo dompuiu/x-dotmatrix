6926809f1bad5e479e3d932115810aaa84981654
dompuiu <dompuiu@gmail.com>
2013-09-20 19:08:01 +0300

    Add new VIM plugins.

--- bin/vimbundles.sh
+++ bin/vimbundles.sh
@@ -29,5 +29,16 @@ get_bundle nathanaelkane vim-indent-guides
 get_bundle scrooloose nerdtree
 get_bundle jistr vim-nerdtree-tabs
 get_bundle bling vim-airline
+get_bundle Lokaltog vim-easymotion
+get_bundle scrooloose syntastic
+get_bundle majutsushi tagbar
+get_bundle sjl vitality.vim
+get_bundle DAddYE html5.vim
+get_bundle hail2u vim-css3-syntax
+get_bundle evanmiller nginx-vim-syntax
+get_bundle terryma vim-multiple-cursors
+get_bundle vim-scripts Emmet.vim
+get_bundle Shougo neocomplcache
+#get_bundle vim-scripts YankRing.vim
 
 vim -c 'call pathogen#helptags()|q'
